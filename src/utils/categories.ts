7f7def593d7c3fcf06846c0704cd2242521273e3
Angelica Herrera <angelicaherreraalba@gmail.com>
2023-05-19 16:26:00 -0500

    New Categories added

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,4 +1,12 @@
 // we export the categories array so other modules can use it.
-const categories = ["Groceries", "Utilities", "Entertainment"] as const; // as const to the end makes the array read only, no modification possible.
+const categories = [
+  "Groceries",
+  "Utilities",
+  "Personal Spending",
+  "Pets",
+  "Healthcare",
+  "Transportation",
+  "Food",
+] as const; // as const to the end makes the array read only, no modification possible.
 
 export default categories;
